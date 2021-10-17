d352dba82c90ca5106db07a64c54f3601865d73c
Vladimir Zaychikov <zaychikov.vl@gmail.com>
2021-10-17 21:57:52 +0300

    nginx config

--- http-server.js
+++ http-server.js
@@ -4,17 +4,17 @@ const PORT = 8080;
 const server = http.createServer((req, res) => {
     console.log(req.url)
 
-    res.setHeader('Link', '<src/styles.css>;rel=preload');
+    res.setHeader('Link', '</src/styles.css>;rel=preload');
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write('<!DOCTYPE html \n');
     res.write('<html> \n')
     res.write('<head> \n')
-    res.write('<link rel=stylesheet type="text/css" href="src/styles.css"> \n')
+    res.write('<link rel="stylesheet" type="text/css" href="/src/styles.css"> \n')
     res.write('</head> \n')
     res.write('<body> \n')
     res.write('<h1>Test</h1> \n')
     res.write('</body> \n')
-    res.write('</html>')
+    res.write('</html>\n')
     res.end()
 })
 
